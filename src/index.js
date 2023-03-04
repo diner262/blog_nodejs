87fe3c1a844fb249d35d88039b7259c2c9fcacc3
din <din090bmt@gmai.com>
2023-03-04 09:03:01 +0700

    fix structure  MVC

--- src/index.js
+++ src/index.js
@@ -1,26 +1,30 @@
-const path = require('path')
-const express = require('express')
-const hdb = require('express-handlebars')
-const app = express()
-const port = 3000
+const path = require('path');
+const express = require('express');
+const hdb = require('express-handlebars');
+const app = express();
+const port = 3000;
+
+const route = require('./routes');
+const db = require('./config/db');
+
+// Connect to DB
+db.connect();
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 //Template engine
 app.engine('hbs', hdb.engine({
   extname: '.hbs'
 }));
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources/views'));
+app.set('views', path.join(__dirname, 'resources', 'views'));
 
-app.get('/', (req, res) => {
-  res.render('home')
-})
-
-app.get('/new', (req, res) => {
-  res.render('new')
-})
+//Route init
+route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`App listening at http://localhost:${port}`)
+});
